d8d7a397614e6a91dda8f1a71c876c470f9ae2c3
Van Huynh <vthuynh@cpp.edu>
2021-09-29 12:49:06 -0700

    plan out program structures in README file

--- Tools.h
+++ Tools.h
@@ -5,15 +5,17 @@
 #include <string.h>
 #include <limits.h>
 
-// Definition in this file, declaration is in Tools_driver.c
+/* Definition in this file, declaration is in Tools_driver.c */
 extern const double GAS_PRICE_PER_MILE;
 
-enum {PARKING_COVERED_PER_DAY = 6,TAXI_COVERED_PER_DAY = 10,HOTEL_COVERED_PER_NIGHT = 90};
-// company allowance
-// const int PARKING_COVERED_PER_DAY = 6;
-// const int TAXI_COVERED_PER_DAY = 10;
-// const int HOTEL_COVERED_PER_NIGHT = 90;
+/* using enum instead of making definition and declaration across 2 files */
+// this is the list of any service covered by the company
+enum {PARKING_COVERED_PER_DAY = 6,TAXI_COVERED_PER_DAY = 10,HOTEL_COVERED_PER_NIGHT = 90,BREAKFAST_COVERED=9, LUNCH_COVERED=12,DINNER_COVERED=16};
 
+/*This way of declaration works normaly in C++ but not in C
+const int PARKING_COVERED_PER_DAY = 6;
+const int TAXI_COVERED_PER_DAY = 10;
+const int HOTEL_COVERED_PER_NIGHT = 90;*/
 int getUserInt(char *prompt); 
 double getUserDouble(char *prompt);
 double getBoundedNum(char *prompt, double lowBound, double highBound);
