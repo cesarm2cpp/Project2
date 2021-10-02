cc9f5d0dec4ad76e00f99bcca24a16c468aaf248
Abigail Pinkus <arpinkus@cpp.edu>
2021-10-02 14:24:49 -0700

    Correct miles

--- calculate.c
+++ calculate.c
@@ -4,107 +4,110 @@
 */  
 
 // Calculate Miles Driven in Private Vehicle
-int calculateMiles() {// $0.27 per mile driven
-    if(carRental == 'y') {
-        miles = miles * 0.27;
+int calculateMiles(int miles, double milesDriven) {// $0.27 per mile driven
+    if(miles > 0) {
+        milesDriven = miles * GAS_PRICE_PER_MILE;
     } else {
-        miles = 0;
+        milesDriven = 0;
     }
-    return miles;
+    return milesDriven;
 }
 
-// Calculate Parking Fees
-int calculateParkingFees() {// company PARKING_COVERED_PER_DAY = 6
-    parkingFees = (parkingFees - PARKING_COVERED_PER_DAY);
-    if(parkingFees < 0) {
-        parkingFees = 0;
-    }
-    return parkingFees;
-}
+// // Calculate Parking Fees
+// int calculateParkingFees(double parkingFees) {// company PARKING_COVERED_PER_DAY = 6
+//     parkingFees = (parkingFees - PARKING_COVERED_PER_DAY);
+//     if(parkingFees < 0) {
+//         parkingFees = 0;
+//     }
+//     return parkingFees;
+// }
 
-// Calculate Taxi Fees
-int calculateTaxiFees() {// company TAXI_COVERED_PER_DAY = 10
-    if(taxiUse == 'y') {
-        taxiFee = (taxiFee - TAXI_COVERED_PER_DAY);
-        if(taxiFee < 0) {
-            taxiFee = 0;
-        }
-    } else {
-        taxiFee = 0;
-    }
+// // Calculate Taxi Fees
+// int calculateTaxiFees(char taxiUse, double taxiFee) {// company TAXI_COVERED_PER_DAY = 10
+//     if(taxiUse == 'y') {
+//         taxiFee = (taxiFee - TAXI_COVERED_PER_DAY);
+//         if(taxiFee < 0) {
+//             taxiFee = 0;
+//         }
+//     } else {
+//         taxiFee = 0;
+//     }
     
-    return taxiFee;
-}
+//     return taxiFee;
+// }
 
-// Calculate Hotel Fees
-int calculateHotelFees() {// company HOTEL_COVERED_PER_NIGHT = 90
-    hotelFee = (hotelFee - HOTEL_COVERED_PER_NIGHT);
-    if(hotelFee < 0) {
-        hotelFee = 0;
-    }
-    return hotelFee;
-}
+// // Calculate Hotel Fees
+// int calculateHotelFees(double hotelFee) {// company HOTEL_COVERED_PER_NIGHT = 90
+//     hotelFee = (hotelFee - HOTEL_COVERED_PER_NIGHT);
+//     if(hotelFee < 0) {
+//         hotelFee = 0;
+//     }
+//     return hotelFee;
+// }
 
-// Calculate Breakfast
-int calculateBreakfast() {// company BREAKFAST_COVERED = 9
-    /* On first day, breakfast is allowed if the time of departure is before 7am. On the last day, breakfast is allowed if arrival is after 8am. */
-    int breakfastNumber = 0;
-    if(departureTime < 7 && arrivalTime > 8) {// on day 1 depature < 7am and arrival on last day > 8am. both
-        breakfastNumber = days;
-    } else if (departureTime < 7 && arrivalTime < 8) {// departure < 7am and arrival < 8am. no arrival breakfast
-        breakfastNumber = days - 1;
-    } else if (departureTime > 7 && arrivalTime > 8) {// departure > 7am and arrival > 8am. no departure breakfast
-        breakfastNumber = days - 1;
-    } else if (departureTime > 7 && arrivalTime < 8) {// 1st day and last day no breakfast. departure > 7am && arrival < 8am
-        breakfastNumber = days - 2;
-    }
-    breakfast = (breakfast - BREAKFAST_COVERED) * breakfastNumber;
-    if(breakfast < 0) {
-        breakfast = 0;
-    }
-    return breakfast;
-}
+// // Calculate Breakfast
+// int calculateBreakfast(double departTime, double arrivalTime, int totalDays) {// company BREAKFAST_COVERED = 9
+//     /* On first day, breakfast is allowed if the time of departure is before 7am. On the last day, breakfast is allowed if arrival is after 8am. */
+//     double breakfast; // company= $9, if(negative): var=0
+//     int breakfastNumber = 0;
+//     if(departureTime < 7 && arrivalTime > 8) {// on day 1 depature < 7am and arrival on last day > 8am. both
+//         breakfastNumber = totalDays;
+//     } else if (departureTime < 7 && arrivalTime < 8) {// departure < 7am and arrival < 8am. no arrival breakfast
+//         breakfastNumber = totalDays - 1;
+//     } else if (departureTime > 7 && arrivalTime > 8) {// departure > 7am and arrival > 8am. no departure breakfast
+//         breakfastNumber = totalDays - 1;
+//     } else if (departureTime > 7 && arrivalTime < 8) {// 1st day and last day no breakfast. departure > 7am && arrival < 8am
+//         breakfastNumber = totalDays - 2;
+//     }
+//     breakfast = (breakfastFees - BREAKFAST_COVERED) * breakfastNumber;
+//     if(breakfast < 0) {
+//         breakfast = 0;
+//     }
+//     return breakfast;
+// }
 
-// Calculate Lunch
-int calculateLunch() {// company LUNCH_COVERED = 12
-    /* On the first day, lunch is allowed if time of departure is before 12pm. On last day, lunch is allowed if the time of arrival is after 1pm */
-    int lunchNumber = 0;
-    if(departureTime < 12 && arrivalTime > 1) {// on day 1 departure < 12pm and arrival on last day > 1pm. both allowed
-        lunchNumber = days;
-    } else if(departureTime < 12 && arrivalTime < 1) {// departure < 12pm and arrival < 1pm. no arrival lunch
-        lunchNumber = days - 1;
-    } else if(departureTime > 12 && arrivalTime > 1) {// departure > 12pm and arrival > 1pm. no departure lunch
-        lunchNumber = days - 1;
-    } else if(departureTime > 12 && arrivalTime < 1) {// 1st day and last day no lunch. departure > 12pm && arrival < 1pm
-        lunchNumber = days - 2;
-    }
-    lunch = (lunch - LUNCH_COVERED) * lunchNumber;
-    if(lunch < 0) {
-        lunch = 0;
-    }
-    return lunch;
-}
+// // Calculate Lunch
+// int calculateLunch(double departTime, double arrivalTime, int totalDays) {// company LUNCH_COVERED = 12
+//     /* On the first day, lunch is allowed if time of departure is before 12pm. On last day, lunch is allowed if the time of arrival is after 1pm */
+//     double lunch; // company= $12, if(negative): var=0
+//     int lunchNumber = 0;
+//     if(departureTime < 12 && arrivalTime > 1) {// on day 1 departure < 12pm and arrival on last day > 1pm. both allowed
+//         lunchNumber = totalDays;
+//     } else if(departureTime < 12 && arrivalTime < 1) {// departure < 12pm and arrival < 1pm. no arrival lunch
+//         lunchNumber = totalDays - 1;
+//     } else if(departureTime > 12 && arrivalTime > 1) {// departure > 12pm and arrival > 1pm. no departure lunch
+//         lunchNumber = totalDays - 1;
+//     } else if(departureTime > 12 && arrivalTime < 1) {// 1st day and last day no lunch. departure > 12pm && arrival < 1pm
+//         lunchNumber = totalDays - 2;
+//     }
+//     lunch = (lunch - LUNCH_COVERED) * lunchNumber;
+//     if(lunch < 0) {
+//         lunch = 0;
+//     }
+//     return lunch;
+// }
 
-// Calculate Dinner
-int calculateDinner() {// company DINNER_COVERED = 16
-    int dinnerNumber = 0;
-    if(departureTime < 6 && arrivalTime > 7) {// on day 1 departure < 6pm and arrival on last day > 7pm. both allowed
-        dinnerNumber = days;
-    } else if(departureTime < 6 && arrivalTime < 7) {// departure < 6pm and arrival < 7pm. no arrival dinner
-        dinnerNumber = days - 1;
-    } else if(departureTime > 6 && arrivalTime > 7) {// depature > 6pm and arrival > 7pm. no departure dinner
-        dinnerNumber = days - 1;
-    } else if(departureTime > 6 && arrivalTime < 7) {// 1st day and last day no dinner. departure > 6pm && arrival < 7pm
-        dinnerNumber = days - 2;
-    }
-    dinner = (dinner - DINNER_COVERED) * dinnerNumber;
-    if(dinner < 0) {
-        dinner = 0;
-    }
-    return dinner;
-}
+// // Calculate Dinner
+// int calculateDinner(double departeTime, double arrivalTime, int totalDays) {// company DINNER_COVERED = 16
+//     double dinner; // company= $16, if(negative): var=0
+//     int dinnerNumber = 0;
+//     if(departureTime < 6 && arrivalTime > 7) {// on day 1 departure < 6pm and arrival on last day > 7pm. both allowed
+//         dinnerNumber = totalDays;
+//     } else if(departureTime < 6 && arrivalTime < 7) {// departure < 6pm and arrival < 7pm. no arrival dinner
+//         dinnerNumber = totalDays - 1;
+//     } else if(departureTime > 6 && arrivalTime > 7) {// depature > 6pm and arrival > 7pm. no departure dinner
+//         dinnerNumber = totalDays - 1;
+//     } else if(departureTime > 6 && arrivalTime < 7) {// 1st day and last day no dinner. departure > 6pm && arrival < 7pm
+//         dinnerNumber = totalDays - 2;
+//     }
+//     dinner = (dinner - DINNER_COVERED) * dinnerNumber;
+//     if(dinner < 0) {
+//         dinner = 0;
+//     }
+//     return dinner;
+// }
 
-// Calculate Meals
-int calculateMealFees() {
-    return calculateBreakfast() + calculateLunch() + calculateDinner();
-}
+// // Calculate Meals
+// int calculateMealFees() {
+//     return calculateBreakfast() + calculateLunch() + calculateDinner();
+// }
