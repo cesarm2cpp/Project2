d95a12d665a8210184712d28f18ac2f2c7d4c421
cesarm2cpp <89374454+cesarm2cpp@users.noreply.github.com>
2021-10-01 22:02:26 -0700

    Update Main.c
    Adding validation for departed and arrival

--- Main.c
+++ Main.c
@@ -24,13 +24,30 @@ int main()
     int hotelDays;  // can be different from totalDays Ex: we can go 4 days and 3 nights
     char carRental[10];
     char taxiUse[10];
+    double minTime = 0.0; //min number is 1
+    double maxTime = 24.0; //max number is 10
     
     printf("Total travel days:");
     scanf("%d", &totalDays);
     
     printf("Enter departure time (0..24 military time):");
     scanf("%lf", &arrivalTime);
-    printf("Enter arrival time (0..24 military time):");
+    do{
+        if(arrivalTime < minTime){
+            printf("\nTime can't be negative");
+            printf("\nEnter departure time (0..24 military time):");
+            scanf("%lf", &arrivalTime);
+        }
+        if(arrivalTime > maxTime)
+        {
+            printf("\nThat time does not exist");
+            printf("\nEnter departure time (0..24 military time):");
+            scanf("%lf", &arrivalTime);
+        }
+    }
+    while(arrivalTime > maxTime || arrivalTime < minTime);
+    
+    printf("\nEnter arrival time (0..24 military time):");
     scanf("%lf", &departTime);
 
     printf("Amount of the round trip airfare: ");
