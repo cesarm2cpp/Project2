ec0f2f673c0f0a76c8a84ee3d35b6a8b7fd5da78
cesarm2cpp <89374454+cesarm2cpp@users.noreply.github.com>
2021-10-01 20:04:28 -0700

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "Tools.h"
+
 int main()
 {
     // doesn't involve different time
@@ -22,37 +22,38 @@ int main()
     double hotelFee;
     int hotelDays;  // can be different from totalDays Ex: we can go 4 days and 3 nights
 
-    totalDays = getUserInt("Please enter total travel days: ");
+    //totalDays = getUserInt("Please enter total travel days: ");
     printf("Total travel days: %d\n", totalDays);
     
-    departTime = getBoundedNum("Enter departure time (0..24 military time) ",0,24);
-    arrivalTime = getBoundedNum("Enter arrival time (0..24 military time) ",0,24);
+    //departTime = getBoundedNum("Enter departure time (0..24 military time) ",0,24);
+    //arrivalTime = getBoundedNum("Enter arrival time (0..24 military time) ",0,24);
     printf("\nConfirm the departure time is: %lf", departTime);
     printf("\nConfirm the arrival time is: %lf", arrivalTime);
 
+    
     char carRental[1];
     char taxiUse[1];
     
     printf("\nAmount of the round trip airfare: ");
-    scanf("%d", &airFare);
+    scanf("%lf", &airFare);
     printf("\nDid you rent any car?(Y or N): ");
     scanf("%s", carRental);
     printf("\nHow many miles did you drive in your private vehicle" );
-    scanf("%d", &miles);
+    scanf("%lf", &miles);
     printf("\nParking fees during trip: ");
-    scanf("%d", &parkingFees);
+    scanf("%lf", &parkingFees);
     printf("\nAmount of days you used the parking: ");
-    scanf("%d", &parkingDays);
+    scanf("%lf", &parkingDays);
     printf("\nDid you use the taxi(Y or N): ");
     scanf("%s", taxiUse);
     printf("\nTaxi fees: ");
-    scanf("%d", &taxiFee);
+    scanf("%lf", &taxiFee);
     printf("\nAmount of days used on taxi: ");
-    scanf("%d", &taxiDays);
+    scanf("%lf", &taxiDays);
     printf("\nConference or seminar registration fees: ");
-    scanf("%d", &confSemFees);
+    scanf("%lf", &confSemFees);
     printf("\nAny hotel expenses: ");
-    scanf("%d", &hotelFee);
+    scanf("%lf", &hotelFee);
 
     return 0;
 }
