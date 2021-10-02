6492584ffe3047d69f1b7cdf97d3182e9578843c
Abigail Pinkus <arpinkus@cpp.edu>
2021-10-02 15:47:01 -0700

    Fixed calculate.h

--- calculate.h
+++ calculate.h
@@ -3,11 +3,11 @@
 
 #include <stdio.h>
 #include <string.h> // if you use a struct or union.
-// #include "Tools.h"
+#include "Tools.h"
 
 const double GAS_PRICE_PER_MILE = 0.27;
 // Enum: User Defined Var
-//enum company{PARKING_COVERED_PER_DAY = 6, TAXI_COVERED_PER_DAY = 10, HOTEL_COVERED_PER_NIGHT = 90, BREAKFAST_COVERED = 9, LUNCH_COVERED = 12, DINNER_COVERED = 16}; // used to hold const vars of company covered fees
+// enum company{PARKING_COVERED_PER_DAY = 6, TAXI_COVERED_PER_DAY = 10, HOTEL_COVERED_PER_NIGHT = 90, BREAKFAST_COVERED = 9, LUNCH_COVERED = 12, DINNER_COVERED = 16}; // used to hold const vars of company covered fees
 
 // // User Vars
 // int getUserInt(char *prompt); 
