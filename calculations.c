7099b0eb23a89d98dcb8055c3a7e18099b776e1b
cesarm2cpp <89374454+cesarm2cpp@users.noreply.github.com>
2021-10-02 10:31:21 -0700

    Update calculations.c

--- calculations.c
+++ calculations.c
@@ -1,2 +1,18 @@
 #include "calculations.h"
 
+depFood(double departTime){
+  if(departTime <  7.0){
+  }
+  if(departTime < 12.0){
+  }
+  if(departTime < 18.00){
+  }
+}
+arrFood(double arrivalTime){
+  if(arrivalTime < 8.0){
+  }
+  if(arrivalTime < 13.0){
+  }
+  if(arrivalTime < 19.0){
+  }
+}
