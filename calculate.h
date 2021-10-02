6bbab1c8a811c123a5bb46b21a83d322323d82f7
Abigail Pinkus <arpinkus@cpp.edu>
2021-10-02 12:42:35 -0700

    Changed Variables and added Tools files

--- calculate.h
+++ calculate.h
@@ -3,18 +3,23 @@
 
 #include <stdio.h>
 #include <string.h> // if you use a struct or union.
+#include "Tools_driver.c"
 
 // Global Calculation Vars
-int days;
-int arrival;
-int departure;
-int roundTrip;
+int totalDays;
+int arrivalTime;
+int departureTime;
+int airFare;
+char carRental; // 'y' or 'n'
 int miles;// $0.27 per mile driven
 int parkingFees;// company= $6*daysParking, if(negative): var=0
-int daysParking;
-int taxi;// company= $10*taxiDays, if(negative): var=0
+int parkingDays;
+char taxiUse; // 'y' or 'n'
+int taxiFee;// company= $10*taxiDays, if(negative): var=0
 int taxiDays;
+int confSemFees; // NOT Currently Used
 int hotelFee;// company= $90*hotelNights, if(negative): var=0
+int hotelDays;
 int meals;// (for allowable meals) breakfast + lunch + dinner
 int breakfast;// company= $9, if(negative): var=0
 int lunch;// company= $12, if(negative): var=0
