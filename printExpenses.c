1332d1def892e7f1d673e49b03ab919a85796d38
cesarm2cpp <89374454+cesarm2cpp@users.noreply.github.com>
2021-10-03 13:41:28 -0700

    Update printExpenses.c
    Added space and titles

--- printExpenses.c
+++ printExpenses.c
@@ -1,27 +1,45 @@
-printf("TOTAl EXPENSES LIST");
-printf("--------------");
-printf("Trip:$");
-printf("Rental:$");
-printf("Mileage:$");
-printf("Parking:$");
-printf("Taxi:$");
-printf("Conference:$");
-printf("Hotels:$");
-printf("Meals:$");
-printf("Total Expenses:$");
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+printf("\nTOTAl EXPENSES LIST");
+printf("\n--------------");
+printf("\nTrip:$");
+printf("\nRental:$");
+printf("\nMileage:$");
+printf("\nParking:$");
+printf("\nTaxi:$");
+printf("\nConference:$");
+printf("\nHotels:$");
+printf("\nMeals:$");
+printf("\nTotal Expenses:$\n");
 
 printf("\nTOTAL ALLOWABLE EXPENSES");
-printf("--------------");
-printf("Trip:$");
-printf("Rental:$");
-printf("Mileage:$");
-printf("Parking:$");
-printf("Taxi:$");
-printf("Conference:$");
-printf("Hotels:$");
-printf("Meals:$");
-printf("Total Expenses:$");
+printf("\n--------------");
+printf("\nTrip:$");
+printf("\nRental:$");
+printf("\nMileage:$");
+printf("\nParking:$");
+printf("\nTaxi:$");
+printf("\nConference:$");
+printf("\nHotels:$");
+printf("\nMeals:$\n");
 
+printf("\nTotal Expenses:$");
+printf("\n--------------");
+printf("\nSavings:$");
 printf("\nTOTAL SAVINGS");
 printf("--------------");
 printf("Savings:$");
+
+
+    return 0;
+}
