1d2ca72fcec22b7f706fd27db0b1effe9816e4f9
cesarm2cpp <89374454+cesarm2cpp@users.noreply.github.com>
2021-10-03 14:41:00 -0700

    Create calculate.h

---
+++ calculate.h
@@ -0,0 +1,10 @@
+#ifndef CALCULATE_H
+#define CALCULATE_H
+
+#include <stdio.h>
+#include <string.h> // if you use a struct or union.
+
+int calcTrip(int totalDays);
+int air(double airFare);
+
+#endif
