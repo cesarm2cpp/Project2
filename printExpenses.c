738b7543e899e0fe78013b689e9b706c04be4707
cesarm2cpp <89374454+cesarm2cpp@users.noreply.github.com>
2021-10-03 13:37:37 -0700

    Create printExpenses.c
    Adding print statements

---
+++ printExpenses.c
@@ -0,0 +1,27 @@
+printf("TOTAl EXPENSES LIST");
+printf("--------------");
+printf("Trip:$");
+printf("Rental:$");
+printf("Mileage:$");
+printf("Parking:$")
+printf("Taxi:$")
+printf("Conference:$")
+printf("Hotels:$")
+printf("Meals:$")
+printf("Total Expenses:$")
+
+printf("TOTAL ALLOWABLE EXPENSES");
+printf("--------------");
+printf("Trip:$");
+printf("Rental:$");
+printf("Mileage:$");
+printf("Parking:$");
+printf("Taxi:$");
+printf("Conference:$");
+printf("Hotels:$");
+printf("Meals:$");
+printf("Total Expenses:$");
+
+printf("TOTAL SAVINGS");
+printf("--------------");
+printf("Savings:$");
