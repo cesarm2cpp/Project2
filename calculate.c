3e4a9f99d23b34cefdb9b891c9d714e7f7d77883
cesarm2cpp <89374454+cesarm2cpp@users.noreply.github.com>
2021-10-03 14:41:22 -0700

    Create calculate.c

---
+++ calculate.c
@@ -0,0 +1,9 @@
+#include "calculate.h"
+
+int calcTrip(int totalDays){
+    int days = totalDays * 100;
+    printf("\nThe price of days is: %d", days);
+}
+int air(double airFare){
+    printf("\nThe Air Fare is the cost ") 
+}
