c7d9a0fe6c3ac7052dbf4d011ce1563ff068cbc8
cesarm2cpp <89374454+cesarm2cpp@users.noreply.github.com>
2021-10-03 17:06:00 -0700

    Update calculate.h

--- calculate.h
+++ calculate.h
@@ -4,7 +4,13 @@
 #include <stdio.h>
 #include <string.h> // if you use a struct or union.
 
-int calcTrip(int totalDays);
-int air(double airFare);
+double calcParking(double parkingFees,int parkingDays);
+double carMileage(double privateCar);
+double tax(double taxiFee, double taxiDays);
+double hotel(double hotelFee, double totalDays);
+double bf(double b, double a, double dep);
+double lch(double l, double a, double dep);
+double dr(double d, double a, double dep);
+double TotalExpense(double airFare, double rentalPrice,double privateValue,double calcPark,double taxiFee,double confSemFees,double hotelFee,double totalFood);
 
 #endif
