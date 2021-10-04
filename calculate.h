10743f7bc95e838e3c044831e278406d2a66ae64
cesarm2cpp <89374454+cesarm2cpp@users.noreply.github.com>
2021-10-03 17:26:39 -0700

    Update calculate.h

--- calculate.h
+++ calculate.h
@@ -12,5 +12,6 @@ double bf(double b, double a, double dep);
 double lch(double l, double a, double dep);
 double dr(double d, double a, double dep);
 double TotalExpense(double airFare, double rentalPrice,double privateValue,double calcPark,double taxiFee,double confSemFees,double hotelFee,double totalFood);
+double Totaltemp(double airFare,double rentalPrice,double privateValue, double confSemFees);
 
 #endif
